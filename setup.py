d7a0ba2bacf520a9af76f94d6d1f0b0b164cf14c
Antoine Trouvé <trouve.antoine@icloud.com>
2023-08-07 14:12:15 +0900

    Update URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='srcfg',
       version='0.1',
       description='A schema-on-read config file format',
-      url='http://github.com/trouve-antoine/srcfg',
+      url='https://github.com/trouve-antoine/srcfg-python',
       author='Antoine Trouvé',
       author_email='contact@antoine-trouve.com',
       license='MIT',
